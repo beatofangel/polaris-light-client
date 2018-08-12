44c1a69ed4d76a7aa23ff5dd1278e6e77ab242a0
beatofangel <beatofangel@gmx.com>
2018-08-13 01:28:59 +0800

    v-charts demo

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import VchartsDemo from '@/components/VchartsDemo'
+import Element from 'element-ui'
 
 Vue.use(Router)
+Vue.use(Element)
 
 export default new Router({
   routes: [
